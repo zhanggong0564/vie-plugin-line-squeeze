a41afd34c1294b506120362d64479d64e5941425
zhanggong <zhanggong1@@sungrowpower.com>
2026-07-17 20:01:36 +0800

    fix: 修正 OCR 模型路径参数以保持一致性

--- vie_plugin_line_squeeze/config.py
+++ vie_plugin_line_squeeze/config.py
@@ -1,6 +1,7 @@
 class LineSqueezeConfig:
     det_model_path = "./weights/line_squeeze/det_v3.onnx"
-    ocr_model_dir = "./weights/common/official/PP-en_rec_ppocr_v5"
+    ocr_model_path = "./weights/line_squeeze/rec_ppocrv5en_v1.onnx"
+    ocr_metadata_path = "./weights/common/official/PP-en_rec_ppocr_v5/inference.yml"
     det_nc = 2
     det_conf_threshold = 0.5
     det_nms_threshold = 0.5
